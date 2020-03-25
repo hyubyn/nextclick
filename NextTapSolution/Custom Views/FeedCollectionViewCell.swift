f30452e5e2fcc84ef29ecd8a18c87f419a8cebb3
hyubyn1 <hyubyn@gmail.com>
2020-03-25 19:47:58 +0700

    Use optional for outlets

--- NextTapSolution/Custom Views/FeedCollectionViewCell.swift
+++ NextTapSolution/Custom Views/FeedCollectionViewCell.swift
@@ -10,26 +10,26 @@ import UIKit
 import SDWebImage
 
 class FeedCollectionViewCell: UICollectionViewCell {
-    @IBOutlet private weak var coverImageView: UIImageView!
-    @IBOutlet private weak var containerView: UIView!
-    @IBOutlet private weak var likeContainerView: UIView!
-    @IBOutlet private weak var userNameLabel: UILabel!
-    @IBOutlet private weak var likeCountLabel: UILabel!
+    @IBOutlet private weak var coverImageView: UIImageView?
+    @IBOutlet private weak var containerView: UIView?
+    @IBOutlet private weak var likeContainerView: UIView?
+    @IBOutlet private weak var userNameLabel: UILabel?
+    @IBOutlet private weak var likeCountLabel: UILabel?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        containerView.backgroundColor = .clear
-        coverImageView.layer.cornerRadius = 5
+        containerView?.backgroundColor = .clear
+        coverImageView?.layer.cornerRadius = 5
     }
 
     func setData(feed: NTFeed) {
-        coverImageView.sd_setImage(with: URL(string: feed.coverSrc), completed: nil)
-        userNameLabel.text = feed.user.displayName
+        coverImageView?.sd_setImage(with: URL(string: feed.coverSrc), completed: nil)
+        userNameLabel?.text = feed.user.displayName
         if feed.likes.count > 0 {
-            likeContainerView.isHidden = false
-            likeCountLabel.text = "\(feed.likes.count)"
+            likeContainerView?.isHidden = false
+            likeCountLabel?.text = "\(feed.likes.count)"
         } else {
-            likeContainerView.isHidden = true
+            likeContainerView?.isHidden = true
         }
     }
 }
